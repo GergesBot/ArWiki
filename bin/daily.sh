cf0d34311ca136ef26dc54b141cbaa4eccf608ed
GergesShamon <gerges.shamoon.dev@gmail.com>
2024-03-08 14:44:03 +0000

    Add the Grammarly Repair task to daily cronjobs

--- bin/daily.sh
+++ bin/daily.sh
@@ -1,3 +1,4 @@
 php ${HOME}/ArWiki/src/RunTask RequestsReviewPages
 php ${HOME}/ArWiki/src/RunTask RequestsReviewEdits
 php ${HOME}/ArWiki/src/RunTask FeaturedContent
+php ${HOME}/ArWiki/src/RunTask GrammarlyRepair
