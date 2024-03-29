cc0290b176c2c2172e207f00505325263a334f8b
GergesShamon <gerges.shamoon.dev@gmail.com>
2024-03-29 17:09:59 +0000

    Update file getPages_GrammarlyRepair.sql

--- assets/SQL/getPages_GrammarlyRepair.sql
+++ assets/SQL/getPages_GrammarlyRepair.sql
@@ -1,12 +1,9 @@
 SELECT p.page_title
 FROM page AS p
-INNER JOIN revision AS r ON p.page_id = r.rev_page
-INNER JOIN templatelinks ON p.page_id = templatelinks.tl_from
-INNER JOIN linktarget ON templatelinks.tl_target_id = linktarget.lt_id
+JOIN revision_userindex AS r ON p.page_id = r.rev_page
 WHERE p.page_is_redirect = 0
 AND p.page_namespace = 0
-AND r.rev_timestamp > DATE_SUB(NOW(), INTERVAL 1 DAY)
-AND linktarget.lt_title NOT LIKE "لا_للأخطاء_الإملائية"
+AND r.rev_timestamp > DATE_SUB(NOW(), INTERVAL 1000 HOUR)
 GROUP BY p.page_title
 ORDER BY r.rev_timestamp DESC
 LIMIT {{LIMIT}}
