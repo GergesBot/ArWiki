f6fc7f4df5fc86ae6729ee03080fa467a3c3adf7
GergesShamon <gerges.shamoon.dev@gmail.com>
2024-04-10 17:02:59 +0000

    pdate DeleteRedirectTalkPages  Maintenence

--- src/Tasks/Maintenence/DeleteRedirectTalkPages.php
+++ src/Tasks/Maintenence/DeleteRedirectTalkPages.php
@@ -19,16 +19,30 @@ class DeleteRedirectTalkPages extends Task
         $this->services->newRevisionSaver()->save($revision, $editInfo);
         $this->log->info("add CSD to ${name}.");
     }
-    private function init(): void {
-        $query = $this->query->getArray(Util::ReadFile(FOLDER_SQL . "/RedirectTalkPages.sql"));
-        $pages = array_column($query, "page_title");
-        foreach ($pages as $page) {
-            $this->addCSD($page);
-        }
+    private function delete(string $name): void {
+        $page = $this->getPage($name);
+        $this->services->newPageDeleter()->delete($page, [
+            "reason" => "تحويلة لصفحة نقاش"
+        ]);
+        $this->log->info("Page ${name} has been deleted.");
+    }
+    public function Deleter(): void {
+        $this->running(function() {
+            $query = $this->query->getArray(Util::ReadFile(FOLDER_SQL . "/RedirectTalkPages.sql"));
+            $pages = array_column($query, "page_title");
+            foreach ($pages as $page) {
+                $this->delete($page);
+                sleep(500);
+            }
+        });
     }
     public function RUN(): void {
         $this->running(function() {
-            $this->init();
+            $query = $this->query->getArray(Util::ReadFile(FOLDER_SQL . "/RedirectTalkPages.sql"));
+            $pages = array_column($query, "page_title");
+            foreach ($pages as $page) {
+                $this->addCSD($page);
+            }
         });
     }
 }
