3e88c2df650404842919be0db0a0019dbb4fc94d
GergesShamon <gerges.shamoon.dev@gmail.com>
2024-08-10 01:59:06 +0300

    Replace null pointer checks with loose equality checks in Wikidata constructor

--- src/Service/Wikidata.php
+++ src/Service/Wikidata.php
@@ -29,11 +29,11 @@ class Wikidata {
         ?string $endpoint = self::endpoint
     ) {
         // Check for null pointer references.
-        if ($auth === null) {
+        if ($auth == null) {
             throw new \InvalidArgumentException('Authentication cannot be null');
         }
 
-        if ($client === null) {
+        if ($client == null) {
             throw new \InvalidArgumentException('HTTP client cannot be null');
         }
 
