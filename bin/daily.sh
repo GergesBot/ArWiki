1aa24e02a3b0e6e448e3495f8d86f2e29a74c365
GergesShamon <gerges.shamoon.dev@gmail.com>
2025-06-21 03:20:45 +0300

    add RemoveShortDescription task to daily script

--- bin/daily.sh
+++ bin/daily.sh
@@ -3,3 +3,4 @@ php ${HOME}/ArWiki/src/RunTask RequestsReviewEdits
 php ${HOME}/ArWiki/src/RunTask FeaturedContent
 php ${HOME}/ArWiki/src/RunTask GrammarlyRepair
 php ${HOME}/ArWiki/src/RunTask Maintenence\\DeleteRedirectTalkPages
+php ${HOME}/ArWiki/src/RunTask RemoveShortDescription
