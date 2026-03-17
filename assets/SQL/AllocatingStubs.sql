41512cc685de737929c67bb318fdb1af92075f8f
GergesShamon <gerges.shamoon.dev@gmail.com>
2026-03-17 18:04:30 +0200

    Refactor SQL queries to use linktarget for category links
    Replace the deprecated cl_to column filtering with linktarget joins (lt_namespace = 14 and lt_title). Updated queries in AllocatingStubs, GetPagesFromCategories, Non-free_images, and PagesWithMissingFiles to align with the normalized MediaWiki database schema.

--- assets/SQL/AllocatingStubs.sql
+++ assets/SQL/AllocatingStubs.sql
@@ -1,12 +1,16 @@
 SELECT
     REPLACE(p.page_title, "_", " ") AS title,
-    REPLACE(REPLACE(REPLACE(GROUP_CONCAT(cl2.cl_to), "_", " "),"بوابة ",""),"/مقالات متعلقة","") AS portals
+    REPLACE(REPLACE(REPLACE(GROUP_CONCAT(lt2.lt_title), "_", " "),"بوابة ",""),"/مقالات متعلقة","") AS portals
 FROM
     page AS p
 JOIN categorylinks AS cl1 ON p.page_id = cl1.cl_from
+JOIN linktarget AS lt1 ON cl1.cl_target_id = lt1.lt_id
 JOIN categorylinks AS cl2 ON p.page_id = cl2.cl_from
+JOIN linktarget AS lt2 ON cl2.cl_target_id = lt2.lt_id
 WHERE p.page_is_redirect = 0
 AND p.page_namespace = 0
-AND cl1.cl_to = "بذرة"
-AND cl2.cl_to LIKE "%/مقالات_متعلقة"
+AND lt1.lt_namespace = 14
+AND lt1.lt_title = "بذرة"
+AND lt2.lt_namespace = 14
+AND lt2.lt_title LIKE "%/مقالات_متعلقة"
 GROUP BY p.page_title
